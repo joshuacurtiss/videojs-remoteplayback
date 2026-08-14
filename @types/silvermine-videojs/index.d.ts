5a46b708ce812ca28b86b3ebd09742e03f16be14
Josh Curtiss <josh@curtiss.me>
2026-08-14 16:44:25 -0500

    feat: Update example to use standard video.js dependency

--- @types/silvermine-videojs/index.d.ts
+++ @types/silvermine-videojs/index.d.ts
@@ -1,7 +1,2 @@
 declare module '*.scss';
-declare module '@silvermine/video.js/dist/*.css';
-
-declare module '@silvermine/video.js' {
-   import videojs from 'video.js';
-   export = videojs;
-}
+declare module 'video.js/dist/*.css';
